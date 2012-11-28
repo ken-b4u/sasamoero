25a40a5be4ebe76bcd19272247def77e0ebe4481
Ken <be4you.jp@gmail.com>
2012-11-28 18:05:00 +0900

    評価ダイヤログの実装

--- src/jp/ne/sakura/sasamori/SasamorieroActivity.java
+++ src/jp/ne/sakura/sasamori/SasamorieroActivity.java
@@ -7,6 +7,8 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -19,11 +21,17 @@ import android.webkit.WebViewClient;
 public class SasamorieroActivity extends Activity {
 	
 	WebView webview = null;
+	SharedPreferences sharedPreferences;
+	private final String EXECUTE_TIME_NAME = "execute_time_name";
+	private final String EXECUTE_TIME_KEY = "exevute_time_key";
+	private final String REVIEW_FLAG_NAME = "review_flag_name";
+	private final String REVIEW_FLAG_KEY = "review_flag_key";
+	
+	private int count = 0;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		// WebViewインスタンスの作成
 		webview = new WebView(this);
 		
@@ -44,6 +52,82 @@ public class SasamorieroActivity extends Activity {
 		
 		// javascriptから呼ばれるファンクションをセット
 		webview.addJavascriptInterface(new JSInterface(), "native_call");
+		
+		saveExecuteTime();
+		
+		if(loadExecuteTime() == 2) {
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			alertDialogBuilder.setTitle("Twitterで情報発信中");
+			alertDialogBuilder.setMessage("ランキング上位の人気ちょいエロアプリや新着アプリを紹介。紹介できない過激アプリも!?");
+			alertDialogBuilder.setPositiveButton("フォローする",
+					new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Uri uri = Uri.parse("https://twitter.com/erozukan");
+					Intent i = new Intent(Intent.ACTION_VIEW,uri);
+					startActivity(i);
+				}
+			});
+			alertDialogBuilder.setNegativeButton("閉じる",
+					new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			alertDialogBuilder.setCancelable(true);
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
+		} else if(loadExecuteTime() % 10 == 0 && loadReviewFlag() == false) {
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			alertDialogBuilder.setTitle("アプリ評価のお願い");
+			alertDialogBuilder.setMessage("皆様からの評価が無料運営のモチベーションになります。よろしくお願いします。");
+			alertDialogBuilder.setPositiveButton("評価する",
+					new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					saveReviewFlag();
+					Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=jp.ne.sakura.sasamori");
+					Intent i = new Intent(Intent.ACTION_VIEW,uri);
+					startActivity(i);
+				}
+			});
+			alertDialogBuilder.setNegativeButton("今はしない",
+					new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			alertDialogBuilder.setCancelable(true);
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
+		}
+	}
+	
+	private void saveReviewFlag() {
+		sharedPreferences = this.getSharedPreferences(REVIEW_FLAG_NAME, MODE_PRIVATE);
+		Editor editor = sharedPreferences.edit();
+		editor.putBoolean(REVIEW_FLAG_KEY, true);
+		editor.commit();
+	}
+	
+	private boolean loadReviewFlag() {
+		sharedPreferences = this.getSharedPreferences(REVIEW_FLAG_NAME, MODE_PRIVATE);
+		return sharedPreferences.getBoolean(REVIEW_FLAG_KEY, false);
+	}
+	
+	private void saveExecuteTime() {
+		sharedPreferences = this.getSharedPreferences(EXECUTE_TIME_NAME, MODE_PRIVATE);
+		count = sharedPreferences.getInt(EXECUTE_TIME_KEY, 0);
+		count++;
+		Editor editor = sharedPreferences.edit();
+		editor.putInt(EXECUTE_TIME_KEY, count);
+		editor.commit();
+	}
+	
+	private int loadExecuteTime() {
+		sharedPreferences = this.getSharedPreferences(EXECUTE_TIME_NAME, MODE_PRIVATE);
+		count = sharedPreferences.getInt(EXECUTE_TIME_KEY, 0);
+		return count;
 	}
 	
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
